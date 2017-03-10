11b798e443b878074e8c40a4ed36fb831e4522e2
alpine9000 <alex.g.brown@gmail.com>
2017-03-10 19:37:37 +1100

    resized map to suit new scrolling parameters

--- game/game.h
+++ game/game.h
@@ -27,7 +27,7 @@
 #define abs(a) ((a) >= 0 ? (a) : -(a))  
 
 #define MAP_TILE_WIDTH      16
-#define MAP_TILE_HEIGHT     202
+#define MAP_TILE_HEIGHT     201
 #define TILE_WIDTH          16
 #define TILE_HEIGHT         16
 
