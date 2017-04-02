31e4e1f6d6127552670f3ab691dd852d985d7649
alpine9000 <alex.g.brown@gmail.com>
2017-04-02 15:20:20 +1000

    test.lua refactor

--- game/test/test.lua
+++ game/test/test.lua
@@ -134,10 +134,8 @@ end
 
 setup = {
    ["startup"] = {
-      next = "startup complete",
       exitState = Setup
-   },
-   ["startup complete"] = {}
+   }
 }
 
 hiscore1 = {
@@ -162,9 +160,7 @@ hiscore1 = {
    ["menu screenshot"] = {
       filename = "test/menu.png",
       transition = Screenshot,
-      next = "done",
-   },
-   ["done"] = {}
+   }
 }
 
 hiscore2 = {
@@ -184,10 +180,8 @@ hiscore2 = {
    },
    ["back to menu"] = {
       writeEntry = {"_script_port", 10},
-      next = "done",
       wait = {"_menu_mode", 1, 32},
-   },
-   ["done"] = {}
+   }
 }
 
 
@@ -195,12 +189,12 @@ level2 = {
    ["booting"] = {
       wait = {"_menu_mode", 1, 32},
       next = "waiting for level to load",
-      write = {"_script_port", string.byte('2')}
+      writeExit = {"_script_port", string.byte('2')}
    },
    ["waiting for level to load"] = {
       wait = {"_menu_mode", 0, 32},      
       next = "waiting for record start",
-      write = {"_script_port", string.byte('P')},
+      writeExit = {"_script_port", string.byte('P')},
    },
    ["waiting for record start"] = {
       wait = {"_record_state", 2, 32},
@@ -224,18 +218,13 @@ level2 = {
    },
    ["verify level parameters"] = {
       less = {{"_game_total", 765648, 32}},
-      equal = {{"_game_score", 13461, 32}, {"_game_lives", 2, 32}},
-      next = "done"
+      equal = {{"_game_score", 13461, 32}, {"_game_lives", 2, 32}}
    },
-   ["done"] = {}      
 }
 
 
 newHiscore = {
    ["booting"] = {
-      next = "game over"
-   },
-   ["game over"] = {
       writeEntry = {"_script_port", string.byte('O')},
       waitFrames = 250,
       next = "ok"
@@ -246,18 +235,18 @@ newHiscore = {
       next = "a"
    },
    ["a"] = {
-      write = {"_script_port", string.byte('a')},
       waitFrames = 250,
+      writeExit = {"_script_port", string.byte('a')},      
       next = "l"
    },
    ["l"] = {
-      write = {"_script_port", string.byte('l')},
       waitFrames = 250,
+      writeExit = {"_script_port", string.byte('l')},
       next = "x"
    },
    ["x"] = {
-      write = {"_script_port", string.byte('x')},
       waitFrames = 250,
+      writeExit = {"_script_port", string.byte('x')},
       next = "screenshot"
    },
    ["screenshot"] = {
@@ -268,22 +257,15 @@ newHiscore = {
    ["enter"] = {
       writeEntry = {"_script_port", 10},
       waitFrames = 250,
-      next = "done"
-   },      
-   ["done"] = {}    
+   }
 }
 
 
 reset = {
    ["booting"] = {
-      next = "reset"
-   },
-   ["reset"] = {
       enterState = Reset,
-      next = "done",
       wait = {"_menu_mode", 0, 32},
-   },
-   ["done"] = {}
+   }
 }
 
 
@@ -298,124 +280,128 @@ tests = {
 }
 
 
+function EnterState(stateMachine, state)
+   stateMachine[state].startFrame = uae_peek_symbol32("_hw_verticalBlankCount") 
+   io.write("enter state ", state, " - frame = ", stateMachine[state].startFrame, "\n")
+   if stateMachine[state].enterState then
+      stateMachine[state].enterState(stateMachine[state])
+   end
+   if  stateMachine[state].writeEntry then
+      Write(stateMachine[state].writeEntry[1], stateMachine[state].writeEntry[2])
+   end
+end
+
+
 function Tick(stateMachine)
    if not quit then 
       local transition = false
       local asserts = false
       
-      if stateMachine[state].next then
-	 if stateMachine[state].equal then
-	    asserts = true
-	    for i, equal in ipairs(stateMachine[state].equal) do
-	       if equal[3] == 32 then
-		  local value = uae_peek_symbol32(equal[1])
-		  if  value == equal[2] then
-		     io.write("PASS: ", equal[1], " ", value, " == ", equal[2], "\n")
-		  else
-		     io.write("FAIL: ", equal[1], " ", value, " != ", equal[2], "\n")
-		     quit = true
-		  end
+      
+      if stateMachine[state].equal then
+	 asserts = true
+	 for i, equal in ipairs(stateMachine[state].equal) do
+	    if equal[3] == 32 then
+	       local value = uae_peek_symbol32(equal[1])
+	       if  value == equal[2] then
+		  io.write("PASS: ", equal[1], " ", value, " == ", equal[2], "\n")
 	       else
-		  local value = uae_peek_symbol16(equal[1])
-		  if (value == equal[2]) then
-		     io.write("PASS: ", equal[1], " ", value, " == ", equal[2], "\n")
-		  else
-		     io.write("FAIL: ", equal[1], " ", value, " != ", equal[2], "\n")
-		     quit = true
-		  end
+		  io.write("FAIL: ", equal[1], " ", value, " != ", equal[2], "\n")
+		  quit = true
 	       end
-	    end
-	 end
-	 
-	 if not quit and stateMachine[state].less then
-	    asserts = true
-	    for i, less in ipairs(stateMachine[state].less) do
-	       if less[3] == 32 then
-		  local val32 = uae_peek_symbol32(less[1])
-		  if val32 < less[2] then
-		     io.write("PASS: ", less[1], " (", val32, ") < ", less[2], "\n")
-		  else
-		     io.write("FAIL: ", less[1], " (", val32, ") >= ", less[2], "\n")
-		     quit = true
-		  end
-	       elseif (uae_peek_symbol16(less[1]) < less[2]) then
+	    else
+	       local value = uae_peek_symbol16(equal[1])
+	       if (value == equal[2]) then
+		  io.write("PASS: ", equal[1], " ", value, " == ", equal[2], "\n")
 	       else
-		  io.write("FAIL: ", less[1], " (",uae_peek_symbol16(less[1]), ") >= ", less[2], "\n")
+		  io.write("FAIL: ", equal[1], " ", value, " != ", equal[2], "\n")
 		  quit = true
 	       end
 	    end
 	 end
-	 
-	 if not quit and asserts == false then
-	    if stateMachine[state].wait then
-	       if stateMachine[state].wait[3] == 32 then
-		  if (uae_peek_symbol32(stateMachine[state].wait[1]) == stateMachine[state].wait[2]) then
-		     io.write("trigger: ", stateMachine[state].wait[1], " == ", stateMachine[state].wait[2], "\n")
-		     transition = true
-		  end
+      end
+      
+      if not quit and stateMachine[state].less then
+	 asserts = true
+	 for i, less in ipairs(stateMachine[state].less) do
+	    if less[3] == 32 then
+	       local val32 = uae_peek_symbol32(less[1])
+	       if val32 < less[2] then
+		  io.write("PASS: ", less[1], " (", val32, ") < ", less[2], "\n")
 	       else
-		  local val16 = uae_peek_symbol16(stateMachine[state].wait[1])
-		  --io.write("read16: ", stateMachine[state].wait[1], " == ", val16, "\n")
-		  if (val16 == stateMachine[state].wait[2]) then
-		     io.write("trigger: ", stateMachine[state].wait[1], " == ", stateMachine[state].wait[2], "\n")
-		     transition = true
-		  end
+		  io.write("FAIL: ", less[1], " (", val32, ") >= ", less[2], "\n")
+		  quit = true
 	       end
-		  
+	    elseif (uae_peek_symbol16(less[1]) < less[2]) then
 	    else
-	       transition = true
-	    end
-	 else
-	    if not quit then
-	       transition = true
+	       io.write("FAIL: ", less[1], " (",uae_peek_symbol16(less[1]), ") >= ", less[2], "\n")
+	       quit = true
 	    end
 	 end
-
-	 if stateMachine[state].waitFrames then
-	    if uae_peek_symbol32("_hw_verticalBlankCount") > (stateMachine[state].startFrame + stateMachine[state].waitFrames) then
-	       io.write("trigger: ",uae_peek_symbol32("_hw_verticalBlankCount"), " > ", stateMachine[state].startFrame, " + ", stateMachine[state].waitFrames, "\n")
-	       transition = true
+      end
+      
+      if not quit and asserts == false then
+	 if stateMachine[state].wait then
+	    if stateMachine[state].wait[3] == 32 then
+	       if (uae_peek_symbol32(stateMachine[state].wait[1]) == stateMachine[state].wait[2]) then
+		  io.write("trigger: ", stateMachine[state].wait[1], " == ", stateMachine[state].wait[2], "\n")
+		  transition = true
+	       end
 	    else
-	       transition = false
-	    end
-	 end
-	 
-	 if not quit and stateMachine[state].transition then
-	    transition = stateMachine[state].transition(stateMachine[state])
-	 end
-	 
-	 if transition then
-	    if stateMachine[state].exitState then
-	       stateMachine[state].exitState()
+	       local val16 = uae_peek_symbol16(stateMachine[state].wait[1])
+	       --io.write("read16: ", stateMachine[state].wait[1], " == ", val16, "\n")
+	       if (val16 == stateMachine[state].wait[2]) then
+		  io.write("trigger: ", stateMachine[state].wait[1], " == ", stateMachine[state].wait[2], "\n")
+		  transition = true
+	       end
 	    end
 	    
-	    if stateMachine[state].writeExit then
+	 else
+	    transition = true
+	 end
+      else
+	 if not quit then
+	    transition = true
+	 end
+      end
+      
+      if stateMachine[state].waitFrames then
+	 if uae_peek_symbol32("_hw_verticalBlankCount") > (stateMachine[state].startFrame + stateMachine[state].waitFrames) then
+	    io.write("trigger: ",uae_peek_symbol32("_hw_verticalBlankCount"), " > ", stateMachine[state].startFrame, " + ", stateMachine[state].waitFrames, "\n")
+	    transition = true
+	 else
+	    transition = false
+	 end
+      end
+      
+      if not quit and stateMachine[state].transition then
+	 transition = stateMachine[state].transition(stateMachine[state])
+      end
+      
+      if transition then
+	 if stateMachine[state].exitState then
+	    stateMachine[state].exitState()
+	 end
+	 
+	 if not quit then 
+	    if  stateMachine[state].writeExit then
 	       Write(stateMachine[state].writeExit[1], stateMachine[state].writeExit[2])
 	    end
-
-	    if not quit then 
-	       if  stateMachine[state].write then
-		  Write(stateMachine[state].write[1], stateMachine[state].write[2])
-	       end
+	    
+	    if stateMachine[state].next then
 	       io.write("state: ", state, " -> ", stateMachine[state].next, "\n")
 	       state = stateMachine[state].next
-	       stateMachine[state].startFrame = uae_peek_symbol32("_hw_verticalBlankCount") 
-	       if stateMachine[state].enterState then
-		  stateMachine[state].enterState(stateMachine[state])
-	       end
-	       if  stateMachine[state].writeEntry then
-		  Write(stateMachine[state].writeEntry[1], stateMachine[state].writeEntry[2])
+	       EnterState(stateMachine, state)
+	    else
+	       test = test + 1
+	       if tests[test] then
+		  io.write("=== next test: ", tests[test][2], " ===\nstate: ", state, " -> ", "booting\n")
+		  state = "booting"
+		  EnterState(tests[test][1], state)
 	       end
 	    end
-	 end	 
-      else
-	 test = test + 1
-	 if tests[test] then
-	    io.write("=== next test: ", tests[test][2], " ===\nstate: ", state, " -> ", "booting\n")
 	 end
-
-	 state = "booting"
-      end
+      end	 
       
       io.flush()
       
